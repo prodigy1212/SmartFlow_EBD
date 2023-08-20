16a1b719bbe66b997f70823366113aa8795faec4
Prodigy1212 <asareharrison72@gmail.com>
2023-08-20 14:16:59 +0000

    Idea of sequence of events

--- control/control.ino
+++ control/control.ino
@@ -8,25 +8,52 @@
 // Create Amplitude Shift Keying Object
 //Note: pin13=relay signal, pin11=receiver signal 
 RH_ASK rf_driver;
+uint8_t buf[20];
+    uint8_t buflen = sizeof(buf);
+    unsigned long previousMillis = 0; // Store the last time an action was performed
+    const unsigned long interval = 10000; // Interval in milliseconds (10 second)
+      
  
 void setup()
 {
   Serial.begin(9600 );
   // Initialize ASK Object
+pinMode(13,OUTPUT);  
+/* from here we check whether environment substation is attached and work, and we also check for GSM module*/
   if (!rf_driver.init()){
-    Serial.println("init failed");  
-     pinMode(13,OUTPUT);
+    Serial.println("init failed"); 
+         
+     exit (1);     
   }
-    
+  int i=0;
+  while( i<=10){
+ if (rf_driver.recv(buf, &buflen)){
+      Serial.println("Received:  ");
+      Serial.println((char *)buf);
+      break;
+    }
+    else {
+      i++;    }
+      delay(2000);
+  }      
+        
+    if (i==11){
+      Serial.println ("Can't from environment ");
+      exit (1);               
+    }  
+    // after checking environment,we check for a connection to the GSM for the first time. if all is well then we enter the loop 
 }
  
 void loop()
 {
-    // Set buffer to size of expected message
     
-    uint8_t buf[20];
-    uint8_t buflen = sizeof(buf);
+ unsigned long currentMillis = millis(); // Get the current time   
+  if (currentMillis - previousMillis >= interval) {
+        // check sensors to see where it is time to irrigate 
+    previousMillis = currentMillis;
+  } 
     
+    /*
     // Check if received packet is correct size
     Serial.println("hello ");
     if (rf_driver.recv(buf, &buflen)){
@@ -42,5 +69,6 @@ void loop()
      
      analogWrite(13,LOW);
      delay(2000);
+   */
 }
    
